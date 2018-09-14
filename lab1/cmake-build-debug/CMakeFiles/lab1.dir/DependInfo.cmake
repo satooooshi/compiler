cd25eb9424c2563c2825fa2946ad2bb09ded4a44
aikawasatoshi <>
2018-09-15 02:19:34 +0800

    first commit

---
+++ lab1/cmake-build-debug/CMakeFiles/lab1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/aikawasatoshi/sceLabs/lab1/main.c" "/Users/aikawasatoshi/sceLabs/lab1/cmake-build-debug/CMakeFiles/lab1.dir/main.c.o"
+  "/Users/aikawasatoshi/sceLabs/lab1/myimpl.c" "/Users/aikawasatoshi/sceLabs/lab1/cmake-build-debug/CMakeFiles/lab1.dir/myimpl.c.o"
+  "/Users/aikawasatoshi/sceLabs/lab1/prog1.c" "/Users/aikawasatoshi/sceLabs/lab1/cmake-build-debug/CMakeFiles/lab1.dir/prog1.c.o"
+  "/Users/aikawasatoshi/sceLabs/lab1/slp.c" "/Users/aikawasatoshi/sceLabs/lab1/cmake-build-debug/CMakeFiles/lab1.dir/slp.c.o"
+  "/Users/aikawasatoshi/sceLabs/lab1/util.c" "/Users/aikawasatoshi/sceLabs/lab1/cmake-build-debug/CMakeFiles/lab1.dir/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab1/cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/main.c.o"
+  "CMakeFiles/lab1.dir/myimpl.c.o"
+  "CMakeFiles/lab1.dir/prog1.c.o"
+  "CMakeFiles/lab1.dir/slp.c.o"
+  "CMakeFiles/lab1.dir/util.c.o"
+  "lab1.pdb"
+  "lab1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
